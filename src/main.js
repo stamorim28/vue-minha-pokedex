00ecb6026988336ac14df7c02f24798f98e2b8d9
Stenio <jstenio.rocha@gmail.com>
2022-08-06 15:02:00 -0300

    feat: pinia on global main file

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from '../pages'
 
 import '@/assets/css/reset.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.mount('#app')
 AOS.init();
