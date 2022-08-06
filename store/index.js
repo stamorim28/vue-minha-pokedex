dc1e8ac549695244803a4367569d0545310ff938
Stenio <jstenio.rocha@gmail.com>
2022-08-06 17:05:03 -0300

    feat: request for pokemon list

--- store/index.js
+++ store/index.js
@@ -1,10 +1,28 @@
 import { defineStore } from 'pinia'
-import state from "./state";
-import getters from "./getters";
-import actions from "./actions";
+import axios from "axios"
+// import state from "./state";
+// import getters from "./getters";
+// import actions from "./actions";
 
 export const usePokedexStore = defineStore('pokedex', {
-  getters,
-  actions,
-  state,
+  state: () => ({ pokemons: [] }),
+  actions: {
+
+      async fetchPokemons() {
+        try {
+          const data = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+            this.pokemons = data.data
+          }
+          catch (error) {
+            alert(error)
+            console.log(error)
+        }
+      }
+  },
 })
+
+// export const usePokedexStore = defineStore('pokedex', {
+//   state,
+//   getters,
+//   actions,
+// })
