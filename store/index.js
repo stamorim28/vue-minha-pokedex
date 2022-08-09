6d5752bad38b2503a6d1f0dbc932ee7422b31ff8
Stenio <jstenio.rocha@gmail.com>
2022-08-09 01:15:36 -0300

    feat: only fetch request for all project

--- store/index.js
+++ store/index.js
@@ -7,25 +7,17 @@ import axios from "axios"
 const resource = "https://pokeapi.co/api/v2/pokemon";
 
 export const usePokedexStore = defineStore('pokedex', {
-  state: () => ({ pokemons: [], pokemonsSkills: [] }),
+  state: () => ({ pokemonsSkills: [] }),
   actions: {
       async fetchPokemons() {
         try {
-            const data = await axios.get(resource + "?limit=151&offset=0")
-            this.pokemons = data.data
-          }
-          catch (error) {
-            alert(error)
-            console.log(error)
-        }
-      },
-      async fetchPokemonsSkills() {
-        try {
+            const skill = []
             const getPokemonUrl = id => (resource + "/" + id)
-            for (let i = 1; i < 150; i++) {
-              this.pokemonsSkills.push(await axios.get(getPokemonUrl(i)))
+            for (let i = 1; i < 152; i++) {
+              skill.push(await axios.get(getPokemonUrl(i)))
             }
-            this.pokemonsSkills = await Promise.all(this.pokemonsSkills)
+            const data = await Promise.all(skill)
+            this.pokemonsSkills = data
           }
           catch (error) {
             alert(error)
