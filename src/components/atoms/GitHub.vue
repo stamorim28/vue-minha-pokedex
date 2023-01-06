4440e2e17d31b355d45f3cec8edebb26d9439bb6
Stenio <jstenio.rocha@gmail.com>
2023-01-05 22:52:30 -0300

    feat: github component

---
+++ src/components/atoms/GitHub.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="github">
+    <a
+      href="https://github.com/stamorim28"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src="../../public/img/gh.png" alt="github" />
+    </a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .github {
+    position: fixed;
+    top: 2%;
+    right: 2%;
+    z-index: 2;
+
+    @media (max-width: map-get($media-breakpoints, "sm")) {
+      & {
+        position: absolute;
+      }
+    }
+  }
+</style>
