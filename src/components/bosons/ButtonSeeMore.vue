7d5885643e45d14590b3680411b5189e406d3723
Stenio <jstenio.rocha@gmail.com>
2023-01-07 18:44:05 -0300

    feat: new component ButtonSeeMore

---
+++ src/components/bosons/ButtonSeeMore.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="button-see-more">
+    {{ title }}
+  </button>
+</template>
+
+<script setup>
+  const props = defineProps({
+    title: String,
+  });
+</script>
+
+<style lang="scss" scoped>
+  .button-see-more {
+    background: $dark-black;
+    border: none;
+    padding: 0.5rem;
+    color: $white;
+    font-weight: bold;
+    cursor: pointer;
+
+    &:hover {
+      filter: drop-shadow(5px 5px $white);
+      transition: all ease 0.3s;
+    }
+  }
+</style>
